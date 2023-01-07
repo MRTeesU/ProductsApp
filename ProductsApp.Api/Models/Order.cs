115e068c2e0c894ba211eb8cdefb81145c1d4b95
MATTHEW RUTLAND <A0072461@live.tees.ac.uk>
2023-01-07 11:57:51 +0000

    added order seed data to context

--- ProductsApp.Api/Models/Order.cs
+++ ProductsApp.Api/Models/Order.cs
@@ -8,9 +8,11 @@
         }
         public Order(int productid, int userid)
         {
-
+            ProductId = productid;
+            UserId = userid;
         }
         public int ProductId { get; set; }
         public int UserId { get; set; }
     }
+
 }
--- ProductsApp.Api/Models/ProductsContext.cs
+++ ProductsApp.Api/Models/ProductsContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProductsApp.Api.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace BaseApp.Models
     {
         public DbSet<Product> Products{ get; set; }
         public DbSet<User> Users{ get; set; }
+        public DbSet<Order> Orders { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options) =>
         options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Products;");
@@ -21,20 +23,25 @@ namespace BaseApp.Models
             base.OnModelCreating(builder);
 
             builder.Entity<Product>().HasData(
-                new Product { Id = 1, Name = "Example Product 1", Description = "Example ",Price = 1 ,InStock = true },
-                new Product { Id = 2, Name = "Example Product 2", Description = "Example ", Price = 2, InStock = true },
-                new Product { Id = 3, Name = "Example Product 3", Description = "Example ", Price = 3, InStock = true },
-                new Product { Id = 4, Name = "Example Product 4", Description = "Example ", Price = 4, InStock = true },
-                new Product { Id = 5, Name = "Example Product 5", Description = "Example ", Price = 5, InStock = true },
-                new Product { Id = 6, Name = "Example Product 6", Description = "Example ", Price = 6, InStock = true }
+                new Product { ProductId = 1, Name = "Example Product 1", Description = "Example ",Price = 1 ,InStock = true },
+                new Product { ProductId = 2, Name = "Example Product 2", Description = "Example ", Price = 2, InStock = true },
+                new Product { ProductId = 3, Name = "Example Product 3", Description = "Example ", Price = 3, InStock = true },
+                new Product { ProductId = 4, Name = "Example Product 4", Description = "Example ", Price = 4, InStock = true },
+                new Product { ProductId = 5, Name = "Example Product 5", Description = "Example ", Price = 5, InStock = true },
+                new Product { ProductId = 6, Name = "Example Product 6", Description = "Example ", Price = 6, InStock = true }
                 );
             builder.Entity<User>().HasData(
-                new User { Id = 1,Name = "Person 1",Role = "Unregistered Customer"},
-                new User { Id = 2, Name = "Person 2", Role = "Registered Customer" },
-                new User { Id = 3, Name = "Person 3", Role = "Staff" },
-                new User { Id = 4, Name = "Person 4", Role = "Management" }
+                new User { UserId = 1, Name = "Person 1", Role = "Unregistered Customer"},
+                new User { UserId = 2, Name = "Person 2", Role = "Registered Customer" },
+                new User { UserId = 3, Name = "Person 3", Role = "Staff" },
+                new User { UserId = 4, Name = "Person 4", Role = "Management" }
                 );
-            //put user and model seed data here after creating respective models.
+            builder.Entity<Order>().HasData(
+                new Order { UserId = 1, ProductId = 1 },
+                new Order { UserId = 2, ProductId = 2 },
+                new Order { UserId = 3, ProductId = 3 },
+                new Order { UserId = 4, ProductId = 4 }
+                );
         }
     }
 }
